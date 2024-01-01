f50aeb00e3b44416cccb70981db574f9dddc3ba5
EthanRiley <gilworthethan@gmail.com>
2024-01-01 16:08:39 -0600

    some minor chaanges

--- mySite/urls.py
+++ mySite/urls.py
@@ -1,8 +1,9 @@
-from django.urls import path
+from django.urls import path, include, re_path
 from . import views
  
 urlpatterns = [
   path('hello',views.say_hello),
   path('home', views.load_homepage),
-  path('reservations', views.load_boarding_form)
+  path('reservations', views.load_boarding_form),
+  path('accounts/', include('django.contrib.auth.urls')),
  ]
