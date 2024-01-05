406988d3a5c262d5757f62e4a79a834dc34bf162
EthanRiley <gilworthethan@gmail.com>
2024-01-04 20:21:12 -0600

    data modeling

--- mySite/models.py
+++ mySite/models.py
@@ -1,3 +1,19 @@
 from django.db import models
+from django.conf import settings
+
 
 # Create your models here.
+class Reservation(models.model):
+    customer = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    startDate = models.DateTimeField()
+    endDate = models.DateTimeField()
+    numDogs = models.IntegerField()
+
+class Blog(models.model):
+    title = models.CharField(max_length=50)
+    body = models.TextField()
+    reservation = models.ForeignKey(Reservation, on_delete=models.CASCADE)
+
+class BlogImage(models.model):
+    imagePath = models.CharField(max_length=100)
+    blog = models.ForeignKey(Blog, on_delete=models.CASCADE)
